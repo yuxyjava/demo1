a1d572e35bee4fa11c2743e0e867efed41f19d61
yuxyjava <yuxyjava@126.com>
2015-11-06 18:46:02 +0800

    我的提交

--- src/org/demo/Student.java
+++ src/org/demo/Student.java
@@ -2,4 +2,25 @@ package org.demo;
 
 public class Student {
 
+	private int id;
+	
+	private String userName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
 }
